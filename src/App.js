9518477f5a481e229b20d8e812a7ca51c2b444ff
jordandescharles <73353820+jordandescharles@users.noreply.github.com>
2022-04-20 16:39:43 +0200

    update

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from "react";
 import { Route, Routes} from "react-router";
 import Home from "./component/router/Homepage"
 import Bio from "./component/router/Bio";
-
-
+ 
 function App() {
   return (
   <Routes>
